5b809ed95afabb339e686c5500676eede8805eb9
akxaalex16 <akxaalex16@gmail.com>
2024-09-07 12:18:41 -0700

    turtle challenge 4 - random walk

---
+++ Day 18/turtle_challenge_4.py
@@ -0,0 +1,21 @@
+from turtle import Turtle, Screen
+import random
+
+bean = Turtle()
+
+directions = [0, 90, 180, 270]
+colors = ["maroon", "floral white", "violet", "sandy brown", "pale green", "pale turquoise", "light slate gray", "gold"]
+# speed = random.randint(0, 10)
+# bean.speed(speed)
+bean.pensize(16)
+bean.speed('fastest')
+
+for _ in range(200):
+    bean.color(random.choice(colors))
+    bean.forward(30)
+    bean.setheading(random.choice(directions))
+
+
+
+screen = Screen()
+screen. exitonclick()
