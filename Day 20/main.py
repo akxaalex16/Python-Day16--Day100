994ab5c7d417b3d4483c11d423da20ddbe96dcd9
akxaalex16 <akxaalex16@gmail.com>
2024-09-18 17:17:52 -0700

    set up screen for snake game project

---
+++ Day 20/main.py
@@ -0,0 +1,16 @@
+from turtle import Turtle, Screen
+
+# create a snake body
+# move the snake
+# control the snake
+# detect collision with food
+# create a scoreboard
+# detect collision with wall to end
+# detect collision with tail to end
+
+screen = Screen()
+screen.setup(width=600, height=600)
+screen.bgcolor('black')
+screen.title('My Snake Game')
+screen.exitonclick()
+
