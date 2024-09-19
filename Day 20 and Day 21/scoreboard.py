51b116ea98c220d1b4c031bb8d5bb3e577ff2dc5
akxaalex16 <akxaalex16@gmail.com>
2024-09-19 14:39:57 -0700

    added functionalities to end the game when colliding with the wall

--- Day 20 and Day 21/scoreboard.py
+++ Day 20 and Day 21/scoreboard.py
@@ -16,6 +16,10 @@ class Scoreboard(Turtle):
     def update_scoreboard(self):
         self.write(f"Score: {self.score}", align=ALIGNMENT, font=FONT)
 
+    def game_over(self):
+        self.goto(0, 0)
+        self.write("GAME OVER", align=ALIGNMENT, font=FONT)
+
     def add_points(self):
         self.score += 1
         self.clear()
