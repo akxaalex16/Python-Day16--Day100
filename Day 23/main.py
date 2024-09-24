f2872747d2aed31832af61b4382f1829d5f38f68
akxaalex16 <akxaalex16@gmail.com>
2024-09-23 18:59:11 -0700

    initial turtle crossing game

---
+++ Day 23/main.py
@@ -0,0 +1,35 @@
+import time
+from turtle import Screen
+from player import Player
+from car_manager import CarManager
+from scoreboard import Scoreboard
+
+
+screen = Screen()
+screen.setup(width=600, height=600)
+screen.tracer(0)
+
+turtle = Player()
+car = CarManager()
+
+screen.listen()
+screen.onkey(turtle.move_across, "Up")
+
+game_is_on = True
+
+while game_is_on:
+    time.sleep(0.1)
+    screen.update()
+
+    car.create_car()
+    car.move_cars()
+
+    #detect collision with car
+    for cars in car.all_cars:
+        if cars.distance(turtle) < 20:
+            game_is_on = False
+            
+
+
+
+screen.exitonclick()
